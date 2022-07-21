8de882831a68495593f5bbf174dc3b6689f3c4a6
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-07-21 10:54:58 +0530

    Create Create2DArray.java

---
+++ Create2DArray.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+class Main{
+    public static void main(String[] args){
+        int[][]  int2DArray;
+        int2DArray = new int[2][2];
+
+        int2DArray[0][0]=1;
+        int2DArray[1][0]=2;
+        int2DArray[0][1]=3;
+        int2DArray[1][1]=4;
+
+        System.out.println(Arrays.deepToString(int2DArray));
+
+        //All together 
+
+        String ritesh[][]= {{"r","i"},{"t","e"},{"s","h"}};
+        System.out.println(Arrays.deepToString(ritesh));
+
+    }
+}
+
+// file name should be Main.java
